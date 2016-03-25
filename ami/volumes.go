19974c6ac2675944471c8b83b7956d95bb29cc41
Marin Bek <marin.bek@gmail.com>
2016-03-25 11:29:35 +0100

    fix imports

--- ami/volumes.go
+++ ami/volumes.go
@@ -7,7 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 
-	. "github.com/KrakenSystems/rumpacker"
+	. "github.com/KrakenSystems/rumpacker/state"
 )
 
 func (job *Job) GetVolumeState() (string, error) {
